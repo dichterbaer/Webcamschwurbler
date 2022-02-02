d403e3882d53210d231e65762fec3a4823706a68
Maximilian Stempel <maximilian.stempel@googlemail.com>
2022-02-02 19:41:55 +0100

    added RotateX and invert

--- webcamschwurbler.py
+++ webcamschwurbler.py
@@ -48,11 +48,52 @@ def CannyBackground(frame, cascadeHandle, height, width):
     return canny
 
 
-def Rotate(image, index):
-  image_center = tuple(np.array(image.shape[1::-1]) / 2)
-  rot_mat = cv2.getRotationMatrix2D(image_center, index%360, 1.0)
-  result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
-  return result
+def FourierJet(frame):
+    frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+    f = np.fft.fft2(frame)
+    fshift = np.fft.fftshift(f)
+    rows, cols = frame.shape
+    crow,ccol = rows//2 , cols//2
+    fshift[crow-30:crow+31, ccol-30:ccol+31] = 0
+    f_ishift = np.fft.ifftshift(fshift)
+    img_back = np.fft.ifft2(f_ishift)
+    img_back = np.real(img_back)
+    return img_back
+
+
+def MirrorY(frame, invert):
+    dims = frame.shape
+    height = dims[0]
+    width = dims[1]
+    flipped = cv2.flip(frame, 1)
+    if invert: 
+        frame[0:height, int(width/2):width] = flipped[0:height, int(width/2):width]
+    else:
+        frame[0:height, 0:int(width/2)] = flipped[0:height, 0:int(width/2)]
+    return frame 
+
+
+def MirrorX(frame, invert):
+    dims = frame.shape
+    height = dims[0]
+    width = dims[1]
+    flipped = cv2.flip(frame, 0)
+    if invert: 
+        frame[int(height/2):height, 0:width] = flipped[int(height/2):height, 0:width]
+    else:
+        frame[0:int(height/2), 0:width] = flipped[0:int(height/2), 0:width]
+    return frame 
+
+    
+
+
+
+def Rotate(image, index, invert):
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    angle = -index%360 if invert else index%360
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+    return result
 
 
 def getElement(event):
@@ -67,23 +108,26 @@ def getElement(event):
 
 root = Tk()
 root.title("Webcamschwurbler")
-root.geometry("500x500")
+root.geometry("180x195")
 
 app = Frame(root)
 app.grid()
 
 stop = Button(app, text="Stop", command=stop)
-stop.grid()
+stop.grid(row=0, column=0)
 
 pause = Button(app, text="pause", command=pause, activebackground='orange')
-pause.grid()
+pause.grid(row=0, column=1)
 
 var2 = StringVar()
-var2.set(('Kein Filter','Canny','Canny BG', 'Rotate'))
+var2.set(('Kein Filter', 'Canny', 'Canny BG', 'Rotate', 'MirrorX', 'MirrorY'))
 lb = Listbox(root, listvariable=var2)
-lb.grid()
+lb.grid(row=1, column=0, columnspan=2)
 lb.bind('<<ListboxSelect>>', getElement) #Select click
 
+invert = IntVar()
+cb_invert = Checkbutton(text='invert', variable=invert)
+cb_invert.grid(row=1, column=3)
 
 vc = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
@@ -109,11 +153,12 @@ with pyvirtualcam.Camera(width, height, fps, fmt=PixelFormat.BGR) as cam:
     print('Virtual camera device: ' + cam.device)
     while True:
         #update gui every 10 frames
-        if idx % 10 == 0:
+        if idx % 1 == 0:
             root.update()
         if not running:
             break
         if pausing:
+            idx += 1
             continue
         
         ret, frame = vc.read()
@@ -125,7 +170,12 @@ with pyvirtualcam.Camera(width, height, fps, fmt=PixelFormat.BGR) as cam:
         elif filter == 2:
             frame = CannyBackground(frame, face_cascade, height, width)
         elif filter == 3:
-            frame = Rotate(frame, idx)
+            frame = Rotate(frame, idx, invert.get())
+            frame = frame
+        elif filter == 4:
+            frame = MirrorX(frame, invert.get())
+        elif filter == 5:
+            frame = MirrorY(frame, invert.get())
         
         cam.send(frame)
         idx += 1
